80e1336add9a0ba4991a186b3d938c78a32ce199
Eddy Proca <eddy@Eddy-Procas-MacBook-Pro.local>
2013-11-28 10:45:08 +0100

    added whitebox tests and changed presentation

--- Docs/1dv404.L01/triangel/Program.cs
+++ Docs/1dv404.L01/triangel/Program.cs
@@ -9,6 +9,9 @@ namespace triangel
 			Console.WriteLine ("Tester av konstruktorn med tre double-parametrar");
 			Console.WriteLine ("╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍");
 
+			Console.WriteLine ("Trianglar");
+			Console.WriteLine ("╌╌╌╌╌╌╌╌╌");
+
 			Triangle test1 = new Triangle (2, 2, 2);
 			Result ("Tre lika positiva tal", test1.isEquilateral ());
 
@@ -22,41 +25,38 @@ namespace triangel
 			Result ("Tre lika positiva tal med decimal", test4.isEquilateral ());
 
 			Triangle test5 = new Triangle (1.5, 1.5, 2.5);
-			Result ("Två lika positiva tal med decimal och ett tredje med decimal", test5.isIsosceles ());
+			Result ("Två lika positiva tal med decimal och ett tredje tal med decimal", test5.isIsosceles ());
 
-			try {
-				Triangle test7 = new Triangle (2, 2, 3);
-				if (IsTriangle (test7)) {
-					Result ("Tre olika positiva tal som inte skapar en triangel", false);
-				} else {
-					Result ("Tre olika positiva tal som inte skapar en triangel", true);
-				}
-			} catch {
-				Result ("Tre olika positiva tal som inte skapar en triangel", true);
-			}
+			Triangle test6 = new Triangle (3.5, 4.5, 5.5);
+			Result ("Tre olika positiva tal med decimal", test6.isScalene ());
 
-			try {
-				Triangle test8 = new Triangle (0, 2, 2);
-				if (IsTriangle (test8)) {
-					Result ("Två lika positiva tal och noll", false);
-				} else {
-					Result ("Två lika positiva tal och noll", true);
-				}
-			} catch {
-				Result ("Två lika positiva tal och noll", true);
-			}
+			Console.WriteLine ();
+			Console.WriteLine ("Icke-trianglar");
+			Console.WriteLine ("╌╌╌╌╌╌╌╌╌╌╌╌╌╌");
+
+			Triangle test7 = new Triangle (1, 2, 3);
+			Result ("Tre olika positiva tal som inte skapar en triangel", !IsTriangle (test7));
+
+			Triangle test8 = new Triangle (0, 1, 1);
+			Result ("Två lika positiva tal och noll", !IsTriangle (test8));
+
+			Triangle test9 = new Triangle (-1, 1, 1);
+			Result ("Två lika positiva tal och ett negativt tal", !IsTriangle (test9));
+
+			Triangle test10 = new Triangle (-1, -1, -1);
+			Result ("Tre lika negativa tal", !IsTriangle (test10));
 		}
 		public static void Result (string test, bool result) {
-			Console.Write (test);
-			Console.Write (": ");
 			if (result) {
 				Console.ForegroundColor = ConsoleColor.Green;
-				Console.WriteLine ("pass");
+				Console.Write ("Pass");
 			} else {
 				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine ("fail");
+				Console.Write ("Fail");
 			}
 			Console.ResetColor ();
+			Console.Write (": ");
+			Console.WriteLine (test);
 		}
 		public static bool IsTriangle (Triangle t) {
 			if (t.isEquilateral () || t.isIsosceles () || t.isScalene ()) {
